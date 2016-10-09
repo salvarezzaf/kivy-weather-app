a8bf96990ccf2a236b036a2595f6a2c3abb4d53b
Fabio Salvarezza <salvarezza.fabio@gmail.com>
2016-10-09 01:29:54 +0100

    v1 Added three day weather forecast at the bottom of the screen

--- locationforecast.py
+++ locationforecast.py
@@ -8,6 +8,7 @@ class LocationForecast(BoxLayout):
     
     wrapper = ObjectProperty()
     util = WeatherUtil()
+    degree_symbol = u"\u00B0"
     
     def __init__(self, **kwargs):
         super(LocationForecast, self).__init__(**kwargs)        
@@ -19,10 +20,13 @@ class LocationForecast(BoxLayout):
         request = UrlRequest(forecast_url_formatted, self.create_forecast_wrapper)
     
     def create_forecast_wrapper(self, request, data):
-        for forecast in data['forecast']['simpleforecast']['forecastday']:
+        for forecast in data['forecast']['simpleforecast']['forecastday'][1:]:
             current_forecast = Factory.DayForecastWrapper()
             current_forecast.forecast_img = self.util.get_icon_for_conditions(forecast['icon'])
             current_forecast.forecast_txt = forecast['conditions']
+            high_temp = str(forecast['high']['celsius'])
+            low_temp = str(forecast['low']['celsius'])
+            current_forecast.hilowtemps = high_temp + self.degree_symbol + "/" +  low_temp + self.degree_symbol  
             current_forecast.day = forecast['date']['weekday']
             self.wrapper.add_widget(current_forecast)
             
