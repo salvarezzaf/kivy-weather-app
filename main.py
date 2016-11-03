bb28df3a4d3cf337f3f3efac33725987fc7d3050
Fabio Salvarezza <fabio.salvarezza@cgi.com>
2016-11-03 16:50:20 +0000

    V4 Refactored application in order to solve the issue of data not updating on screens

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
-from kivy.factory import Factory
-from currentconditions import CurrentCondition
 from kivy.utils import get_color_from_hex
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
-from locationforecast import LocationForecast
 from dbutil import DbUtil
 from addlocation import AddLocation
-from kivy.uix.screenmanager import ScreenManager, Screen
 
-Builder.load_file("kvFiles/currentconditions.kv")
+#Builder.load_file("kvFiles/currentconditions.kv")
 Builder.load_file("kvFiles/buttons.kv")
-Builder.load_file("kvFiles/locationforecast.kv")
+#Builder.load_file("kvFiles/locationforecast.kv")
 Builder.load_file("kvFiles/addlocation.kv")
 
-Factory.register("CurrentCondition", cls=CurrentCondition)
-Factory.register("LocationForecast", cls=LocationForecast)
-Factory.register("AddLocation", cls=AddLocation)
-
-class CurrentConditionsScreen(Screen):
-    pass
-
-class AddLocationScreen(Screen):
-    pass
+#Factory.register("CurrentCondition", cls=CurrentCondition)
+#Factory.register("LocationForecast", cls=LocationForecast)
+#Factory.register("AddLocation", cls=AddLocation)
 
 
-class YamaRoot(ScreenManager):
+class YamaRoot(BoxLayout):
     pass
 
 
