67373e5dfd21c8db4b79ded1ed0e1c8fd46b35a5
Fabio Salvarezza <fabio.salvarezza@cgi.com>
2016-12-21 12:14:00 +0000

    V5 added mock for kivy windowing during tests execution

--- test/test_currentconditions.py
+++ test/test_currentconditions.py
@@ -1,9 +1,12 @@
 import unittest
 import urllib
 
+from dateutil.parser import parser
+from kivy.base import EventLoopBase
 from kivy.network.urlrequest import UrlRequest
 from mock import patch, MagicMock
 import mock
+from mock.mock import MagicMock
 
 from app.currentconditions import CurrentCondition
 from app.dbutil import DbUtil
@@ -15,34 +18,30 @@ from test.testutils import TestUtils
 class TestCurrentConditions(unittest.TestCase):
 
     def setUp(self):
-        self.current_conditions = CurrentCondition()
         self.test_utils = TestUtils()
+        self.test_utils.start_kivy_windowing_stub()
+        self.current_conditions = CurrentCondition()
         self.data = self.test_utils.load_json_from_file("test/glasgow_conditions_response.json")
         self.forecast = self.test_utils.load_json_from_file("test/forecast.json")
         self.degree_symbol = u"\u00B0"
-        self.current_temp = '3'+ self.degree_symbol
+        self.current_temp = '3' + self.degree_symbol
         self.highlow_temps = '11' + self.degree_symbol + '/' + '6' + self.degree_symbol
 
-    @patch.object(DbUtil, 'get_default', MagicMock(return_value=Location(location_name="Glasgow",
-                                                                                 location_code="UK",
-                                                                                 location_lat="12.334",
-                                                                                 location_long="55,345",
-                                                                                 isDefault=True)))
-    @patch.object(DbUtil,'get_default_tem_unit', MagicMock(return_value="celsius"))
+    @patch.object(DbUtil, 'get_default', MagicMock(
+        return_value=Location(location_name="Glasgow", location_code="UK", location_lat="12.334",
+                              location_long="55,345", isDefault=True)))
+    @patch.object(DbUtil, 'get_default_tem_unit', MagicMock(return_value="celsius"))
     @patch.object(WeatherUtil, 'get_icon_for_conditions', MagicMock(return_value="app/img/clear.png"))
     @mock.patch('app.currentconditions.UrlRequest')
-    def test_get_current_conditions(self,mock_url_request):
+    def test_get_current_conditions(self, mock_url_request):
         mock_url_request = MagicMock()
-        mock_url_request.side_effect = [self.current_conditions.parse_current_weather_data(mock_url_request.request,self.data),
-                                        self.current_conditions.parse_high_low_temperature(mock_url_request.request,self.forecast)]
+        mock_url_request.side_effect = [
+            self.current_conditions.parse_current_weather_data(mock_url_request.request, self.data),
+            self.current_conditions.parse_high_low_temperature(mock_url_request.request, self.forecast)]
         self.current_conditions.get_current_conditions()
         self.assertEqual(str(self.current_conditions.current_location_name), "Glasgow,UK")
         self.assertEqual(str(self.current_conditions.current_condition_text), "Clear")
         self.assertEqual(str(self.current_conditions.current_condition_img), "app/img/clear.png")
         self.assertEqual(self.current_conditions.current_temp, self.current_temp)
         self.assertEqual(self.current_conditions.high_low_temps, self.highlow_temps)
-
-
-
-
 
--- test/testutils.py
+++ test/testutils.py
@@ -1,10 +1,26 @@
 import json
 
+import mock
+from kivy.base import EventLoopBase
+
 
 class TestUtils():
 
-    def load_json_from_file(self,filename):
+    def __init__(self):
+        self.mock_patches = [
+           mock.patch('kivy.uix.widget.Builder'),
+           mock.patch.object(EventLoopBase, 'ensure_window', lambda x: None),
+         ]
 
+    def load_json_from_file(self, filename):
         with open(filename) as f:
             data = json.load(f)
         return data
+
+    def start_kivy_windowing_stub(self):
+        for patch in self.mock_patches:
+            patch.start()
+
+    def stop_kivy_windowing_stub(self):
+        for patch in self.mock_patches:
+            patch.stop()
